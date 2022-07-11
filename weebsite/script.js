bd0799873cb691145956a7d06060062f4140da11
Caleb Westaway <calebwestaway@icloud.com>
2022-07-11 20:54:47 +0100

    Things are happening!
    Changes to site, started C++

---
+++ weebsite/script.js
@@ -0,0 +1,10 @@
+// This is a comment
+function hideShowB1() {
+    var x = document.getElementById("buttonContentB1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
